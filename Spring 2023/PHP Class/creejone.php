59ebbfeb077b8e7cf1eddfe7243e95f3fd565932
Goldencode20 <avatarcreed1@gmail.com>
2023-02-23 10:48:44 -0500

    Daily Push

--- Spring 2023/PHP Class/creejone.php
+++ Spring 2023/PHP Class/creejone.php
@@ -1,46 +1,43 @@
 <?php
 
-//$userText = $_GET["text"];
-
-//echo (pi() * $_GET["text"] ** 2);
-
-//echo " Square inches";
-
-//circlearea($_GET[8]);
-
 $userInput = $_GET["text"];
 
+if ($userInput == NULL) { //Incase the user doesn't input anything
+    $url = 'https://xkcd.com/info.0.json';
+    $curlHandle = curl_init($url);
+    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($curlHandle);
+    $resultArray = json_decode($result, true);  
+    echo "I see you didn't add anything. Please try again and add a number between 0 and " .  $resultArray["num"] . " at the end.";
+    exit;
+}
+
 $url = 'https://xkcd.com/' . $userInput . '/info.0.json';
-
 $curlHandle = curl_init($url);
-
 curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-
 $result = curl_exec($curlHandle);
-
 $resultArray = json_decode($result, true);
 
-//Here can I check if the user input was valid
-
-//What does an error 404 return?
-if ($resultArray == NULL) {
-    
+if ($resultArray == NULL) { //Check if the user did not upload a number higher than the current highest comic
     $url = 'https://xkcd.com/info.0.json';
-
     $curlHandle = curl_init($url);
-
     curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-
     $result = curl_exec($curlHandle);
-
     $resultArray = json_decode($result, true);  
-    
-    echo "Here is the newest comic becuase you entered a value above the newest comic. Feel free to try again with a number under this number " . $resultArray["num"];
-} else {
-    echo "Here is the link to the comic " . $userInput . ": " . $resultArray["img"];
+    echo "You entered a value above the newest comic. Feel free to try again with a number between 0 and  " . $resultArray["num"];
+} else { //The code were it takes in the user input and returns the img
+    echo "Here is comic " . $userInput;
+    //echo "Here is the link to the comic " . $userInput . ": " . $resultArray["img"];
 }
-// check if error 404 is returned and if it is return newset comic and the number of it
 
-//var_dump($resultArray);
+$responseURL = $_GET["response_url"];
+//echo $responseURL;
 
+$payload = ["type" => "image", "image_url" => $resultArray["img"]];
+$payloadJSON = json_encode($payload);
+$curlHandleSlack = curl_init($responseURL);
+curl_setopt($curlHandleSlack, CURLOPT_POST, true);
+curl_setopt($curlHandleSlack, CURLOPT_POSTFIELDS, $payloadJSON);
+curl_setopt($curlHandleSlack, CURLOPT_RETURNTRANSFER, true);
+curl_exec($curlHandleSlack);
 ?>  
